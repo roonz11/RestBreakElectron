716106e78c3c5e8032c68a5359e3dfde02ce45f6
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-15 12:25:30 -0800

    app now runs in electron window;

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, Notification } = require("electron");
+const url = require("url");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL(`file://${(path.join(__dirname, `/dist/RestBreakElectron/index.html`))}`);
+
+}
+
+const NOTIFICATION_TITLE = "Basic Notification HOT";
+const NOTIFICATION_BODY = "Notification from the Main process";
+
+function showNotification() {
+  new Notification({ title: NOTIFICATION_TITLE, body: NOTIFICATION_BODY }).show();
+}
+
+app.whenReady()
+  .then(createWindow)
+  .then(showNotification);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/app/services/notification.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// const electron = (<any>window).require('electron');
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class NotificationService {
+
+//   constructor() { 
+//     electron.ipcRender.on('showNotificationResponse', (event: any, result: any) => {
+//       console.log('yo yo yo', result);
+//     });
+//   }
+// }
