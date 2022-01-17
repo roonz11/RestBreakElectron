0896a5a0ca8438ad2680c8adae93e01664aa29ed
Aruna Meiyeppen <aruna.meiyeppen@digitalistgroup.com>
2022-01-16 20:44:45 -0800

    fixed routing issue;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
 
   constructor(private router: Router) {
-    router.navigate(['dashboard']);
+    //router.navigate(['dashboard']);
   }
 
   title = 'Rest Break Electron';
--- src/services/heart-beat.service.ts
+++ src/services/heart-beat.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { BehaviorSubject, interval } from 'rxjs';
+import { BehaviorSubject, interval, Observable } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 import * as dayjs from 'dayjs'
 @Injectable({
   providedIn: 'root'
 })
 export class HeartBeatService {
   public restIntervalSubject: BehaviorSubject<dayjs.Dayjs> = new BehaviorSubject<dayjs.Dayjs>(dayjs());
-  private restInterval: any;
+  private restInterval: Observable<number> = interval(environment.restInterval);
+  private intervalElapsed: boolean = false;
   constructor() { }
 
   public start (): void {
     console.log(environment.restInterval)
-    this.restInterval = interval(environment.restInterval)
-      .subscribe(val => this.callHeartBeat());
+    this.restInterval
+      .pipe(takeWhile(() => !this.intervalElapsed))
+      .subscribe(() => this.callHeartBeat());
   }
 
   public stop (): void {
-    this.restInterval = null;
+    this.intervalElapsed = true;
   }
 
   private callHeartBeat (): void {
